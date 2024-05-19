15249ab347cc8ab05ba020669223d77cbb6d89a9
Tymoshenko Dmytro <tzd0409@gmail.com>
2024-05-20 01:15:52 +0300

    WIP

--- internal/api/tests/api_test.go
+++ internal/api/tests/api_test.go
@@ -6,85 +6,25 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 	"strings"
 	"testing"
+	"time"
 
-	"github.com/gorilla/mux"
 	"github.com/proj-go-5/accounts/internal/api"
 	"github.com/proj-go-5/accounts/internal/api/dto"
 	"github.com/proj-go-5/accounts/internal/entities"
-	store "github.com/proj-go-5/accounts/internal/repositories"
-	"github.com/proj-go-5/accounts/internal/services"
-	"github.com/proj-go-5/accounts/pkg/authorization"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 type TestService struct {
-	Router *mux.Router
-
-	AdminService *services.Admin
-	HashService  *services.Hash
-	EnvService   *services.Env
-	CacheService *services.Cache
-	JwtServcie   *authorization.JwtService
-	AppServcie   *services.AppService
-
-	EnvFilePath string
 }
 
 func (s *TestService) initServices() error {
-	filename := ".env"
-	s.EnvFilePath = filename
-
-	os.WriteFile(filename, []byte(""), 0644)
-
-	envService, err := services.NewEnvService(filename)
-	if err != nil {
-		return err
-	}
-	s.EnvService = envService
-
-	hashService := services.NewHashService()
-	s.HashService = hashService
-	adminService := services.NewAdminService(store.NewUserMemoryRepository(), hashService)
-	s.AdminService = adminService
-	cacheService := services.NewCacheService(store.NewMemoryCacheRepository())
-	s.CacheService = cacheService
-
-	jwtSecret := envService.Get("JWT_SECRET", "secret")
-	jwtExpiration, err := strconv.Atoi(envService.Get("JWT_EXPIRATION_HOURS", "24"))
-	if err != nil {
-		return err
-	}
-
-	jwtService := authorization.NewJwtService(jwtSecret, jwtExpiration)
-	s.JwtServcie = jwtService
-
-	appService := &services.AppService{
-		Admin: adminService,
-		Jwt:   jwtService,
-		Cache: cacheService,
-		Auth: services.NewAuthService(
-			adminService, cacheService, jwtService, hashService,
-		),
-	}
-	s.AppServcie = appService
 	return nil
 }
 
 func (s *TestService) initRouter() error {
-	a := api.New(s.AppServcie)
-
-	router, err := a.CreateRouter()
-	if err != nil {
-		return err
-	}
-
-	s.Router = router
-
 	return nil
 }
 
@@ -102,7 +42,6 @@ func (s *TestService) SetUp() error {
 }
 
 func (s *TestService) CleanUp() {
-	defer os.Remove(s.EnvFilePath)
 }
 
 func TestCreateAdminOk(t *testing.T) {
@@ -111,20 +50,28 @@ func TestCreateAdminOk(t *testing.T) {
 	defer testService.CleanUp()
 
 	if err != nil {
+		return
+	}
+
+	app, err := api.NewApp()
+	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
 	go func() {
-		if err := http.ListenAndServe(":8888", testService.Router); err != nil {
-			log.Printf("Server run error: %s", err)
-		}
+		app.Start()
 	}()
+
+	assert.Eventually(t, func() bool {
+		_, err := http.Get("http://localhost:8081")
+		return err == nil
+	}, time.Second, 10*time.Millisecond)
 
 	adminLogin := "test"
 	adminPassword := "test"
 
-	testService.AdminService.Save(&entities.AdminWithPassword{
+	app.Service.Admin.Save(&entities.AdminWithPassword{
 		Login:    adminLogin,
 		Password: adminPassword,
 	})
@@ -136,7 +83,7 @@ func TestCreateAdminOk(t *testing.T) {
 	data, err := json.Marshal(requestData)
 	require.NoError(t, err)
 
-	req, err := http.NewRequest(http.MethodPost, "http://localhost:8888/api/v1/auth/admins/login/", bytes.NewReader([]byte(data)))
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8081/api/v1/auth/admins/login/", bytes.NewReader([]byte(data)))
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -147,6 +94,8 @@ func TestCreateAdminOk(t *testing.T) {
 	require.NoError(t, err)
 
 	assert.Equal(t, resp.StatusCode, http.StatusOK)
+
+	app.Stop()
 }
 
 func TestCreateAdminFail(t *testing.T) {
@@ -159,11 +108,20 @@ func TestCreateAdminFail(t *testing.T) {
 		return
 	}
 
+	app, err := api.NewApp()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
 	go func() {
-		if err := http.ListenAndServe(":8888", testService.Router); err != nil {
-			log.Printf("Server run error: %s", err)
-		}
+		app.Start()
 	}()
+
+	assert.Eventually(t, func() bool {
+		_, err := http.Get("http://localhost:8081")
+		return err == nil
+	}, time.Second, 10*time.Millisecond)
 
 	adminLogin := "test"
 	adminPassword := "test"
@@ -175,7 +133,7 @@ func TestCreateAdminFail(t *testing.T) {
 	data, err := json.Marshal(requestData)
 	require.NoError(t, err)
 
-	req, err := http.NewRequest(http.MethodPost, "http://localhost:8888/api/v1/auth/admins/login/", bytes.NewReader([]byte(data)))
+	req, err := http.NewRequest(http.MethodPost, "http://localhost:8081/api/v1/auth/admins/login/", bytes.NewReader([]byte(data)))
 	if err != nil {
 		log.Fatal(err)
 		return
@@ -191,4 +149,6 @@ func TestCreateAdminFail(t *testing.T) {
 	bodyBytes, err := io.ReadAll(resp.Body)
 	require.NoError(t, err)
 	assert.True(t, strings.Contains(string(bodyBytes), "wrong login or password"))
+
+	app.Stop()
 }
